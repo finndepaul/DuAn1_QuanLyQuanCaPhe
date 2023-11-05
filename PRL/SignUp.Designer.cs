45e7e133a04b13a5b3f3932dc85fd01c26b10c22
isora666 <thqtr666@gmail.com>
2023-11-05 22:14:19 +0700

    Edit form login

--- PRL/SignUp.Designer.cs
+++ PRL/SignUp.Designer.cs
@@ -43,9 +43,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(34, 235);
+            label1.Location = new Point(30, 176);
             label1.Name = "label1";
-            label1.Size = new Size(70, 28);
+            label1.Size = new Size(57, 21);
             label1.TabIndex = 0;
             label1.Text = "UserID";
             // 
@@ -53,35 +53,38 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(34, 289);
+            label2.Location = new Point(30, 217);
             label2.Name = "label2";
-            label2.Size = new Size(93, 28);
+            label2.Size = new Size(76, 21);
             label2.TabIndex = 1;
             label2.Text = "Password";
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(151, 235);
+            textBox1.Location = new Point(132, 176);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(279, 34);
+            textBox1.Size = new Size(245, 29);
             textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
             textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox2.Location = new Point(151, 289);
+            textBox2.Location = new Point(132, 217);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(279, 34);
+            textBox2.Size = new Size(245, 29);
             textBox2.TabIndex = 3;
             textBox2.UseSystemPasswordChar = true;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.Logo;
-            pictureBox1.Location = new Point(125, 12);
+            pictureBox1.Location = new Point(109, 9);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(245, 112);
+            pictureBox1.Size = new Size(214, 84);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 4;
             pictureBox1.TabStop = false;
@@ -90,19 +93,20 @@
             // 
             Login.AutoSize = true;
             Login.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
-            Login.Location = new Point(189, 144);
+            Login.Location = new Point(165, 108);
             Login.Name = "Login";
-            Login.Size = new Size(103, 46);
+            Login.Size = new Size(84, 37);
             Login.TabIndex = 0;
             Login.Text = "Login";
             // 
             // btn_Cancel
             // 
-            btn_Cancel.BackColor = Color.Turquoise;
-            btn_Cancel.ForeColor = SystemColors.ControlText;
-            btn_Cancel.Location = new Point(161, 430);
+            btn_Cancel.BackColor = Color.Crimson;
+            btn_Cancel.ForeColor = Color.White;
+            btn_Cancel.Location = new Point(223, 271);
+            btn_Cancel.Margin = new Padding(3, 2, 3, 2);
             btn_Cancel.Name = "btn_Cancel";
-            btn_Cancel.Size = new Size(176, 39);
+            btn_Cancel.Size = new Size(154, 29);
             btn_Cancel.TabIndex = 2;
             btn_Cancel.Text = "Cancel";
             btn_Cancel.UseVisualStyleBackColor = false;
@@ -111,18 +115,19 @@
             // 
             btn_Login.BackColor = Color.Turquoise;
             btn_Login.ForeColor = SystemColors.ControlText;
-            btn_Login.Location = new Point(161, 365);
+            btn_Login.Location = new Point(30, 271);
+            btn_Login.Margin = new Padding(3, 2, 3, 2);
             btn_Login.Name = "btn_Login";
-            btn_Login.Size = new Size(176, 39);
+            btn_Login.Size = new Size(154, 29);
             btn_Login.TabIndex = 2;
             btn_Login.Text = "Login";
             btn_Login.UseVisualStyleBackColor = false;
             // 
             // SignUp
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(504, 498);
+            ClientSize = new Size(441, 374);
             Controls.Add(pictureBox1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
@@ -131,6 +136,7 @@
             Controls.Add(label2);
             Controls.Add(Login);
             Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "SignUp";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "SignUp";
