a5c66f29c1c3a2b991cc996990883f74e9643cec
bui duy dong <dongbui.work@gmail.com>
2023-11-05 21:53:20 +0700

    create form login

---
+++ PRL/SignUp.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PRL
+{
+    partial class SignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            pictureBox1 = new PictureBox();
+            Login = new Label();
+            btn_Cancel = new Button();
+            btn_Login = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(34, 235);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 28);
+            label1.TabIndex = 0;
+            label1.Text = "UserID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(34, 289);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(151, 235);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(279, 34);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(151, 289);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(279, 34);
+            textBox2.TabIndex = 3;
+            textBox2.UseSystemPasswordChar = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Logo;
+            pictureBox1.Location = new Point(125, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(245, 112);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            Login.AutoSize = true;
+            Login.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            Login.Location = new Point(189, 144);
+            Login.Name = "Login";
+            Login.Size = new Size(103, 46);
+            Login.TabIndex = 0;
+            Login.Text = "Login";
+            // 
+            // btn_Cancel
+            // 
+            btn_Cancel.BackColor = Color.Turquoise;
+            btn_Cancel.ForeColor = SystemColors.ControlText;
+            btn_Cancel.Location = new Point(161, 430);
+            btn_Cancel.Name = "btn_Cancel";
+            btn_Cancel.Size = new Size(176, 39);
+            btn_Cancel.TabIndex = 2;
+            btn_Cancel.Text = "Cancel";
+            btn_Cancel.UseVisualStyleBackColor = false;
+            // 
+            // btn_Login
+            // 
+            btn_Login.BackColor = Color.Turquoise;
+            btn_Login.ForeColor = SystemColors.ControlText;
+            btn_Login.Location = new Point(161, 365);
+            btn_Login.Name = "btn_Login";
+            btn_Login.Size = new Size(176, 39);
+            btn_Login.TabIndex = 2;
+            btn_Login.Text = "Login";
+            btn_Login.UseVisualStyleBackColor = false;
+            // 
+            // SignUp
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(504, 498);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(btn_Cancel);
+            Controls.Add(btn_Login);
+            Controls.Add(label2);
+            Controls.Add(Login);
+            Controls.Add(label1);
+            Name = "SignUp";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SignUp";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private PictureBox pictureBox1;
+        private Label Login;
+        private Button btn_Cancel;
+        private Button btn_Login;
+    }
+}
