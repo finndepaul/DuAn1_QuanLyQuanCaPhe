196ac5410f02c8d4865096e44e3ef0abb508b39a
isora666 <thqtr666@gmail.com>
2023-11-07 23:20:41 +0700

    Edit form login

--- PRL/f_Home.cs
+++ PRL/f_Home.cs
@@ -6,5 +6,20 @@ namespace PRL
         {
             InitializeComponent();
         }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox4_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
